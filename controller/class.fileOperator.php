22e5a34506597e532265f81e07834c4bacc8f521
Kurvin Hendricks <Kurvin Hendricks>
2017-08-31 22:51:42 +0200

    Adding comments

--- controller/class.fileOperator.php
+++ controller/class.fileOperator.php
@@ -3,20 +3,12 @@
 class fileOperator {
 
 	function __contruct() {}
-
-	public function openFile($filePath) {
-		$file = fopen($filePath, "r") or die;
-		return $file;
-	}
-
-	public function readFile($file, $filePath) {
-		return fread($file, filesize($filePath)) or die;
-	}
-
-	public function closeFile($file) {
-		return fclose($file) or die;
-	}
-
+	
+	/**
+	* returns different values based on flag,
+	* either sum of all values in 3rd column
+	* or largest value when suming 2nd and 3rd column
+	*/
 	public function runThroughCSV($filePath, $returnType) {
 
 		$firsRow = $result = $trimIncorrectColumnNumber
@@ -39,7 +31,7 @@ class fileOperator {
 		    	$trimIncorrectColumnNumber[] = $this->checkNumberOfColums($row, 3);
 		    }
 
-		    $trimIncorrectColumnNumber = array_filter($trimIncorrectColumnNumber);
+		    $trimIncorrectColumnNumber = array_filter($trimIncorrectColumnNumber); //removing null values
 
     	} else {
     		echo 'No valid data was found in this file<br>';
@@ -53,7 +45,7 @@ class fileOperator {
     			$trimIncorrectAlphaNums[] = $this->checkAlphaNumberic($row);
     		}
 
-    		$trimIncorrectAlphaNums = array_filter($trimIncorrectAlphaNums);
+    		$trimIncorrectAlphaNums = array_filter($trimIncorrectAlphaNums); //removing null values
     	} else {
     		echo 'No valid data was found in this file';
     		return false;
@@ -70,7 +62,7 @@ class fileOperator {
     			}
     		}
     		
-    		$trimIncorrectTimeStamps = array_filter($trimIncorrectTimeStamps);
+    		$trimIncorrectTimeStamps = array_filter($trimIncorrectTimeStamps); //removing null values
     	} else {
     		echo 'No valid data was found in this file';
     		return false;
@@ -84,18 +76,25 @@ class fileOperator {
     		return $this->calculateSumOfRow($trimIncorrectTimeStamps);
     	}
 
-
-    	return false;
+    	return false; //returns false if failure occurs
     	 	
 	}
 
-	protected function checkNumberOfColums($row, $allowedNumberOfRows) {
-		if(count($row) === $allowedNumberOfRows) {
+	/**
+	* validates number of coulms in CSV file
+	* 2 argurments, row of data and allowed number of columns
+	*/
+	protected function checkNumberOfColums($row, $allowedNumberOfColumns) {
+		if(count($row) === $allowedNumberOfColumns) {
 			return $row;
 		}
 		return null;
 	}
 
+	/**
+	* checks whether given data set, 
+	* has number, and alphabetical
+	*/
 	protected function checkAlphaNumberic($row) {
 		// ABCD-123
 		$alphanumericCheck = $row[0];
@@ -109,6 +108,9 @@ class fileOperator {
 		return null;
 	}
 
+	/**
+	* validates timestamp
+	*/
 	protected function checkValidTimeStamp($timestamp) {
 		if(is_numeric($timestamp)) {
 			return true;
@@ -117,6 +119,9 @@ class fileOperator {
 		
 	}
 
+	/**
+	* returns sum or rows, last column
+	*/
 	protected function calculateSumOfRow($validDataSet) {
 		$sumOfFinalRow = 0;
 
@@ -127,6 +132,9 @@ class fileOperator {
 		return $sumOfFinalRow;
 	}
 
+	/**
+	* returns highest value of sum of columns 2 and 3
+	*/
 	protected function calculateHighestValue($validDataSet) {
 		$highestValue = 0;
 		$currentValue = 0;
